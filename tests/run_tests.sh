fc9d870244447dc445d4a784672badb68bcf76f7
Tim Welch <tim.j.welch@gmail.com>
2022-12-23 14:51:32 -0800

    update test

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -4,6 +4,6 @@ echo
 echo "proj" $(proj 2>&1 | head -n 1)
 echo "geos" $(geos-config --version)
 gdalinfo --version
-gdalinfo --formats | grep OpenJPEG
+ogrinfo --formats | grep FlatGeobuf
 
 exit 0
